941e65cf40ab1dc3ad581d371693a1a95b14d9e5
Matthew Sottile <mjsottile@gmail.com>
2014-12-11 16:44:40 -0800

    updates to hashtag parsing and removal from token list

--- src/talkparse.py
+++ src/talkparse.py
@@ -6,6 +6,7 @@
 #
 import nltk
 import csv
+import sys
 
 class TalkParser(object):
     def __init__(self):
@@ -16,6 +17,8 @@ class TalkParser(object):
            with something that talks to the backing database containing the
            actual data.
         """
+        csv.field_size_limit(sys.maxsize)
+
         with open(fname, 'r') as csvfile:
             talkreader = csv.reader(csvfile)
 
@@ -44,21 +47,29 @@ class TalkParser(object):
            the text is tokenized and stopwords are removed.
         """
         hashtags = []
+        tokens_no_hashtags = []
 
-        # parse hashtags with simple string split
-        words = text.split()
-        for word in words:
-            if word[0] == '#':
-                hashtags.append(word)
+        # tokenize the text, all lowercase
+        tokens = nltk.WordPunctTokenizer().tokenize(text.lower())
 
-        results = {}
-                
-        results['hashtags'] = hashtags
+        # assumption: None will only occur as the tail of the second list.
+        skip_next = False
+        for (t1,t2) in zip(tokens,tokens[1:]+[None]):
+            if skip_next:
+                skip_next = False
+                continue
+            
+            if t2 is None:
+                tokens_no_hashtags.append(t1)
+            elif t1 == "#":
+                hashtags.append(t2)
+                skip_next = True
+            else:
+                tokens_no_hashtags.append(t1)
+        
+        filtered_tokens = [t for t in tokens_no_hashtags if not t in self.stopset]
 
-        tokens = nltk.WordPunctTokenizer().tokenize(text)
-        filtered_tokens = [t for t in tokens if not t in self.stopset]
-
-        results['tokens'] = filtered_tokens
+        results = {'hashtags':hashtags, 'tokens': filtered_tokens}
 
         return results
     
